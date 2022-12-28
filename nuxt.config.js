76cc042e47bbba776ec2fd76f3e9aa9e238a6d08
MaryChiodelli <marychiodelli97@gmail.com>
2022-12-28 19:26:59 +0100

    edit content + style in drink page

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,7 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap'}
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@600&family=Poppins:wght@400;500;600&display=swap'}
     ]
   },
 
--- pages/drinks/_id.vue
+++ pages/drinks/_id.vue
@@ -4,36 +4,40 @@
       <div class="w-1/2 pr-5">
         <img :src="drink.strDrinkThumb" :alt="drink.strDrink">
       </div>
-      <div class="w-1/2">
-        <ul class="flex">
-          <li>Home</li>
+      <div class="w-1/2 py-20 px-32">
+        <ul class="flex mb-10 text-sm leading-normal">
+          <li class="after:content-['/'] after:mx-2">Home</li>
           <li>{{ drink.strDrink }}</li>
         </ul>
-        <h1>{{ drink.strDrink }}</h1>
-        <h2>Instructions:</h2>
-        <ul>
-          <li>{{ drink.strInstructionsIT }}</li>
+        <h1 class="mb-9 text-5xl leading-normal font-semibold">{{ drink.strDrink }}</h1>
+        <h2 class="mb-4 text-2xl leading-normal font-semibold">Instructions:</h2>
+        <ul class="mb-9 list-disc	list-inside">
+          <li v-for="(info, index) in instructions" :key="'info' + index">{{ info }}</li>
         </ul>
-        <h2>Glass:</h2>
+        <h2 class="mb-4 text-2xl leading-normal font-semibold">Glass:</h2>
         <p>Serve: {{ drink.strGlass }}</p>
       </div>
     </section>
-    <section>
-      <h2>Ingredients(4)</h2>
-      <ul>
-        <li>
-          <div>
-            <img src="" alt="ingredient">
-            <div>Ingredient name</div>
-          </div>
-        </li>
-      </ul>
+    <section class="py-10">
+      <div class="w-1/2 pr-5 text-center">
+        <h2 class="mb-10 text-2xl leading-9 font-semibold">Ingredients ({{ ingredients.length }})</h2>
+        <ul class="grid grid-cols-2 gap-2">
+          <li class="w-full aspect-square flex flex-col justify-center items-center bg-gray" v-for="(ingredient, index) in ingredients" :key="index">
+            <img class="w-[175px] mb-10" :src="`https://www.thecocktaildb.com/images/ingredients/${ingredient}-Medium.png`" :alt="ingredient">
+            <div>{{ ingredient }}</div>
+          </li>
+        </ul>
+      </div>
     </section>
-    <section>
+    <section class="pt-10 pb-20">
       <div class="wrapper">
-        <h2>Title</h2>
-        <ul>
-          <li>card</li>
+        <h2 class="mb-20 text-2xl leading-9 font-semibold text-center">Browse more</h2>
+        <ul class="flex -mx-5">
+          <li class="w-1/4 px-5" v-for="drink in moreDrinks" :key="drink.idDrink">
+            <NuxtLink :to="{ name: 'drinks-id', params: { id: drink.idDrink } }">
+              <DrinkCard :drink="drink" />
+            </NuxtLink>
+          </li>
         </ul>
       </div>
     </section>
@@ -44,7 +48,15 @@
 export default {
   async asyncData({ params, $axios }) {
     const { drinks } = await $axios.$get(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${params.id}`)
-    return { drink: drinks[0] }
+    const drink = drinks[0]
+    const ingredients = []
+    const instructions = drink.strInstructions.split('. ')
+    for (let i = 1; i <= 15; i++) {
+      const item = drink[`strIngredient${i}`]
+      if (item) ingredients.push(item)
+    }
+    const moreDrinks = await $axios.$get(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredients[0]}`)
+    return { drink, instructions, ingredients, moreDrinks: moreDrinks.drinks.slice(0, 4) }
   }
 }
 </script>
